4fb75ad963eab10b44a700efcda58fe63d2f36a6
mirellaalves <mirella.alves@gmail.com>
2020-05-12 17:14:35 -0300

    Installing react dom and changing way of create search bar.

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -3,12 +3,10 @@ import React, { Component } from 'react';
 class SearchBar extends Component {
   render() {
     return (
-      <form>
-        <label data-testid="home-initial-message" htmlFor="searchText">
-          Digite algum termo de pesquisa ou escolha uma categoria.
-        <input type="text" id="searchText" value={ } />
-        </label>
-      </form>
+      <div>
+        <input type="text" id="searchText" />
+        <p data-testid="home-initial-message">Digite algum termo de pesquisa ou escolha uma categoria.</p>
+      </div>
     );
   }
 }
