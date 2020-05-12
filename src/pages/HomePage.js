4504a9a3d05ff2ec09723e022f26ce6c30a76d2c
mirellaalves <mirella.alves@gmail.com>
2020-05-12 16:02:53 -0300

    Creating search bar.

---
+++ src/pages/HomePage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+  render() {
+    <form>
+      <label data-testid="home-initial-message" htmlFor="searchText">Digite algum termo de pesquisa ou escolha uma categoria.
+      <input type="text" id="searchText" value={home-initial-message} />
+      </label>
+    </form>
+  }
+}
+
+export default SearchBar;
