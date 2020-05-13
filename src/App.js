c618adffb8e8442fa38a54711f1895701de7d59f
Jafet Henrique <jafetguerra6@gmail.com>
2020-05-12 21:26:11 -0300

    bot problem

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
 import React from 'react';
-<<<<<<< HEAD
 import { BrowserRouter as Router, 
   Route,
   Switch } from 'react-router-dom';
-=======
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
->>>>>>> c37fe95bbff1544937661e1ca4cb23c620146fb8
 import './App.css';
 import SearchBar from './pages/HomePage';
 import FilterCategory from './components/FilterCategory';
