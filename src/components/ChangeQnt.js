afefd55dd6006169a1706e2f2a9f53dc7e586b0d
lcdlima <leticia.duarte.lima@gmail.com>
2020-05-14 17:26:20 -0300

    Able to update total price multiplying quantity but only when it increases

--- src/components/ChangeQnt.js
+++ src/components/ChangeQnt.js
@@ -4,29 +4,52 @@ import './ChangeQnt.css';
 class ChangeQnt extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { qnt: 1 };
+    this.state = { 
+      qnt: 1, 
+      price: this.props.price,
+    };
     this.reduceQnt = this.reduceQnt.bind(this);
     this.increaseQnt = this.increaseQnt.bind(this);
+    this.changePrice = this.changePrice.bind(this);
+  }
+
+  changePrice() {
+    this.setState((state) => ({ price: this.props.price * state.qnt }));
   }
 
   reduceQnt() {
     if (this.state.qnt > 1) {
       this.setState((state) => ({ qnt: state.qnt - 1 }));
+      this.changePrice();
+      this.props.onChange(this.state.price);
     }
   }
 
   increaseQnt() {
     if (this.state.qnt < this.props.max) {
       this.setState((state) => ({ qnt: state.qnt + 1 }));
+      this.changePrice();
+      this.props.onChange(this.state.price);
+    }
+  }
+
+  componentDidMount() {
+    if (this.props.onChange) {
+      this.props.onChange(this.state.price);
     }
   }
 
   render() {
     return (
-      <div className="changeQtnComp">
-        <button data-testid="product-decrease-quantity" onClick={this.reduceQnt}> - </button>
-        <p>{this.state.qnt}</p>
-        <button data-testid="product-increase-quantity" onClick={this.increaseQnt}> + </button>
+      <div className='qntPriceComp'>
+        <div className="changeQtnComp">
+          <button data-testid="product-decrease-quantity" onClick={this.reduceQnt}> - </button>
+          <p>{this.state.qnt}</p>
+          <button data-testid="product-increase-quantity" onClick={this.increaseQnt}> + </button>
+        </div>
+        <div>
+          <p>{`R$${this.state.price.toFixed(2)}`}</p>
+        </div>
       </div>
     );
   }
--- src/components/ProductInCart.js
+++ src/components/ProductInCart.js
@@ -1,17 +1,31 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './ProductInCart.css';
-import ProductCardInCart from './ProductCardInCart';
+import ChangeQnt from './ChangeQnt';
 import SumPrice from '../components/SumPrice';
 
 class ProductInCart extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { totalPrice: 0 }
+    this.changeTotal = this.changeTotal.bind(this);
+  }
+
+  changeTotal(value) {
+    this.setState((state) => ({totalPrice: state.totalPrice + value}))
+  }
+
   render() {
     return (
       <div>
         {this.props.products.map((product) =>
-          <ProductCardInCart product={product} />,
+          <div className="product" key={product.id}>
+            <img src={product.thumbnail} alt="Product" />
+            <p>{product.title}</p>
+            <ChangeQnt max={product.available_quantity} price={product.price} onChange={this.changeTotal}/>
+          </div>,
         )}
-        <p>Valor Total da Compra: R$<SumPrice products={this.props.products} /></p>
+        <p>Valor Total da Compra: R${this.state.totalPrice.toFixed(2)}</p>
         <Link> Finalizar Compra </Link>
       </div>
     );
