0943ac8a9b001afd56997e3422ed5c55bf220b39
mirellaalves <mirella.alves@gmail.com>
2020-05-15 01:23:26 -0300

    Going on working on add-to-cart button.

--- src/components/AddToCart.js
+++ src/components/AddToCart.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import { Redirect } from 'react-router';
+// import ProductCard from './ProductCard';
 
 class AddToCart extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { redirect: false };
+    this.state = {
+      redirect: false,
+    };
     this.goToCart = this.goToCart.bind(this);
   }
 
   goToCart() {
+    // <ProductCard key={product.id} product={product} />;
     this.setState({ redirect: true });
   }
 
