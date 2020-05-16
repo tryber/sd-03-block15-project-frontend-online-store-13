fe3249f93725a30f0ab300174f9e0ca68f0aaef2
Jafet Henrique <jafetguerra6@gmail.com>
2020-05-16 15:20:36 -0300

    codeclimate

--- src/handleChangeFunc.js
+++
@@ -1,8 +0,0 @@
-handleChange(event) {
-  // const { name, value } = event.target;
-  // const { onChange } = this.props;
-  // onChange(name, value);
-  console.log(event);
-}
-
-export default handleChange;
