6c05225e2084b6af4be27c98fd42dbe0cfe43e0e
lcdlima <leticia.duarte.lima@gmail.com>
2020-05-17 09:10:17 -0300

    Adjusting button add to cart in home page to control quantity

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -5,17 +5,46 @@ import './ProductList.css';
 class ProductList extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { session: [] };
+    this.state = { session: [] }
     this.addToSession = this.addToSession.bind(this);
+    this.alreadyHere = this.alreadyHere.bind(this);
+  }
+
+  alreadyHere(toAdd) {
+    const currentList = this.state.session;
+    currentList.forEach((item) => {
+      if (item.product.id === toAdd.product.id) {
+        return {
+          product: toAdd.product,
+          qnt: item.qnt + 1,
+        };
+      }
+      return toAdd;
+    })
   }
 
   addToSession(product, qnt) {
-    const toAdd = {
+    let toAdd = {
       product,
       qnt,
     };
+    if(this.state.session.length !== 0) {
+      const currentList = this.state.session;
+      currentList.forEach((item, index) => {
+      if (item.product.id === toAdd.product.id && item.product.available_quantity >= item.qnt + 1) {
+        currentList[index] = {
+          product: toAdd.product,
+          qnt: item.qnt + 1,
+        };
+        this.setState((state) => ({ session: [...currentList] }));
+        localStorage.setItem('cart', JSON.stringify([...currentList]));
+      }
+    })
+    } else {
     this.setState((state) => ({ session: [...state.session, toAdd] }));
     localStorage.setItem('cart', JSON.stringify([...this.state.session, toAdd]));
+    }
+    
   }
 
   render() {
